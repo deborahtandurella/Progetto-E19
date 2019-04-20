c77f3891abbd846561600ec2f40b20e5a7f3c789
JustCobra <48448671+JustCobra@users.noreply.github.com>
2019-04-20 11:11:54 +0200

    aggiunto speed x

---
+++ flappy/src/logic/SinglePlayer/Player.java
@@ -0,0 +1,14 @@
+package logic.SinglePlayer;
+
+public class Player {
+
+    private int nHearts;
+    private int nCoins;
+    private boolean immunity;
+
+    public Player(){
+        this.nHearts = 3;
+        this.nCoins = 0;
+        this.immunity = false;
+    }
+}
