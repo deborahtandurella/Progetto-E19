8265df566e2e041dddac4b01e770afdfbb25bee1
JustCobra <48448671+JustCobra@users.noreply.github.com>
2019-04-20 11:11:34 +0200

    aggiunto speed x

--- flappy/src/logic/gameElements/Heart.java
+++ flappy/src/logic/gameElements/Heart.java
@@ -5,15 +5,16 @@ import org.newdawn.slick.geom.Shape;
 import java.util.ArrayList;
 
 public class Heart extends GameElement implements SolidElement {
+    private final double speedX;
 
-   public Heart(double x, double y){
+   public Heart(double x, double y, double speedX){
         super(x,y);
-
+        this.speedX = speedX;
    }
 
     @Override
     public void update(int delta) {
-
+        setX(getX() - speedX);
     }
 
     @Override
--- flappy/src/logic/gameElements/Pipe.java
+++ flappy/src/logic/gameElements/Pipe.java
@@ -5,14 +5,16 @@ import org.newdawn.slick.geom.Shape;
 import java.util.ArrayList;
 
 public class Pipe extends GameElement implements SolidElement {
+    private final double speedX;
 
-    public Pipe(double x, double y){
+    public Pipe(double x, double y, double speedX){
         super(x, y);
+        this.speedX = speedX;
     }
 
     @Override
     public void update(int delta) {
-
+        setX(getX() - speedX);
     }
 
 
