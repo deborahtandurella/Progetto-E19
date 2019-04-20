121c80fc71cfa386851b0422ec77c89ea30ccfda
JustCobra <48448671+JustCobra@users.noreply.github.com>
2019-04-20 12:40:01 +0200

    spritesheet

--- flappy/src/graphics/SpriteDrawer.java
+++ flappy/src/graphics/SpriteDrawer.java
@@ -1,23 +1,39 @@
 package graphics;
 
 import logic.gameElements.Bird;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 
 public class SpriteDrawer{
     private Image bird;
+    private Image coin;
+    private Image heart;
+    private SpriteSheet coinSheet;
+    private Animation coinAnimation;
+    private SpriteSheet heartSheet;
+    private Animation heartAnimation;
 
-    {
+    public SpriteDrawer(){
         try {
             bird = new Image("res/bird.png");
+            heart = new Image("res/heart_full.png");
+            coin = new Image("res/onecoin.png");
+
+            coinSheet = new SpriteSheet("res/Coin.png",32,32);
+            coinAnimation = new Animation(coinSheet,100);
+
+            heartSheet = new SpriteSheet("res/Items_Heart.png", 32, 32);
+            heartAnimation = new Animation(heartSheet, 100);
 
         } catch (SlickException e) {
             e.printStackTrace();
         }
     }
 
-    public static drawBird(Bird bird, Graphics graphics){
+
+
+
+
+    public void drawBird(Bird bird, Graphics graphics){
         graphics.drawImage(bird.getX(), bird.getY(),);
 
     }
