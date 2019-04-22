752161fe1c8a0ca04aa89309b41f755831ad22fe
fGuareschi <federico.guareschi01@universitadipavia.it>
2019-04-22 15:19:07 +0200

    fix costanti final

--- flappy/src/logic/gameConstants/GameConstants.java
+++ flappy/src/logic/gameConstants/GameConstants.java
@@ -1,17 +1,18 @@
 package logic.gameConstants;
 
 public class GameConstants {
-    public static double ACCELERATION_Y = 0.0000042f;
-    public static double JUMP_SPEED = 0.0012f;
+    public static final double ACCELERATION_Y = 0.0000042f;
+    public static final double JUMP_SPEED = 0.0012f;
     public static final double BIRD_WIDTH = 0.15;
     public static final double BIRD_HEIGHT = 0.1;
 
     public static final double COIN_SIZE = 0.1;
     public static final double HEART_SIZE = 0.1;
+
     public static final double PIPE_FREE_SPACE = 0.35;
     public static final double PIPE_WIDTH = 1/6d;
     public static final double PIPE_HEIGHT= 0.846;
-    public static double PIPE_SPEED = 0.00052f;
-    public static double PIPE_VERTICAL_SPEED = 0.00018f;
+    public static final double PIPE_SPEED = 0.00052f;
+    public static final double PIPE_VERTICAL_SPEED = 0.00018f;
 
 }
