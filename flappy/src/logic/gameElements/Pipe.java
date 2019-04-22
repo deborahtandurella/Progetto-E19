59db6d82740011cc0042120b4cdf4776015ae9a2
fGuareschi <federico.guareschi01@universitadipavia.it>
2019-04-22 14:34:20 +0200

    collisione pipe

--- flappy/src/logic/gameElements/Pipe.java
+++ flappy/src/logic/gameElements/Pipe.java
@@ -1,9 +1,12 @@
 package logic.gameElements;
 
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 import java.util.ArrayList;
 
+import static logic.gameConstants.GameConstants.PIPE_FREE_SPACE;
+import static logic.gameConstants.GameConstants.PIPE_HEIGHT;
 import static logic.gameConstants.GameConstants.PIPE_WIDTH;
 
 public class Pipe extends GameElement implements SolidElement {
@@ -15,11 +18,14 @@ public class Pipe extends GameElement implements SolidElement {
         this.speedX = speedX;
         setX(x);
         setY(centerY);
+        addHitboxShape(new Rectangle((float) x, (float)(x+PIPE_WIDTH), (float) (centerY-PIPE_HEIGHT-PIPE_FREE_SPACE/2d), (float) PIPE_HEIGHT));
+        addHitboxShape(new Rectangle((float) x, (float)(x+PIPE_WIDTH), (float) (centerY+PIPE_FREE_SPACE/2d), (float) (PIPE_HEIGHT + PIPE_FREE_SPACE/2)));
 
     }
 
     @Override
     public void update(int delta) {
+        shiftHitbox(-speedX*delta, 0);
         setX(getX() - speedX*delta);
     }
 
