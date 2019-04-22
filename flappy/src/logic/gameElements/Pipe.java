e2bfe461bdc48f3c4dc4296135cdb93e19cab62b
fGuareschi <federico.guareschi01@universitadipavia.it>
2019-04-22 12:36:27 +0200

    fix dei pipes

--- flappy/src/logic/gameElements/Pipe.java
+++ flappy/src/logic/gameElements/Pipe.java
@@ -4,13 +4,18 @@ import org.newdawn.slick.geom.Shape;
 
 import java.util.ArrayList;
 
+import static logic.gameConstants.GameConstants.PIPE_WIDTH;
+
 public class Pipe extends GameElement implements SolidElement {
     private final double speedX;
 
 
-    public Pipe(double x, double y, double speedX){
-        super(x, y);
+    public Pipe(double x, double centerY, double speedX){
+        super(x, centerY);
         this.speedX = speedX;
+        setX(x);
+        setY(centerY);
+
     }
 
     @Override
