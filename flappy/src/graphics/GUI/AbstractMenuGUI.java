b5869bead3805a98deb05360e2768437e3145858
fGuareschi <federico.guareschi01@universitadipavia.it>
2019-04-25 15:44:08 +0200

    fix di AbstractMenuGUI

--- flappy/src/graphics/GUI/AbstractMenuGUI.java
+++ flappy/src/graphics/GUI/AbstractMenuGUI.java
@@ -1,14 +1,8 @@
 package graphics.GUI;
 
-import org.newdawn.slick.Game;
 import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.gui.AbstractComponent;
 import org.newdawn.slick.gui.ComponentListener;
-import org.newdawn.slick.state.BasicGameState;
-import org.newdawn.slick.state.GameState;
-import org.newdawn.slick.state.transition.FadeInTransition;
-import org.newdawn.slick.state.transition.FadeOutTransition;
+
 
 
 public abstract class AbstractMenuGUI implements ComponentListener {
@@ -16,5 +10,5 @@ public abstract class AbstractMenuGUI implements ComponentListener {
     public AbstractMenuGUI(GameContainer container) {
         this.container = container;
     }
-
+    public abstract void render();
 }
