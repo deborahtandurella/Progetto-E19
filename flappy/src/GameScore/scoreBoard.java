d11ace7d91bb4e680c2ebe8d7a5e27e812fe7256
JustCobra <48448671+JustCobra@users.noreply.github.com>
2019-05-28 09:24:14 +0200

    classe classifica

---
+++ flappy/src/GameScore/scoreBoard.java
@@ -0,0 +1,31 @@
+package GameScore;
+
+import java.util.HashMap;
+
+public class scoreBoard {
+
+  private String[] playerNames;
+  private int[] scores;
+
+  public scoreBoard(){
+      this.playerNames = new String[10];
+      this.scores = new int[10];
+  }
+
+
+
+
+
+
+    public void loadRecord(){}
+    public boolean isRecord(int score){
+      return false;
+  }
+    public void saveRecord(int score){
+      String player;
+
+    }
+
+
+
+}
