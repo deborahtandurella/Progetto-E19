0f18a3936f67b4008438ac52ae6f47c0c1f3b0a7
simonetartarotti <simone.tartarotti01@universitadipavia.it>
2019-05-31 19:37:29 +0200

    aggiunta classe gui per il customMenu

---
+++ flappy/src/graphics/GUI/CustomizationMenuGUI.java
@@ -0,0 +1,21 @@
+package graphics.GUI;
+
+import graphics.Screen;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.gui.AbstractComponent;
+
+public class CustomizationMenuGUI extends AbstractMenuGUI {
+    public CustomizationMenuGUI(GameContainer container, Screen screen) {
+        super(container, screen);
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void componentActivated(AbstractComponent abstractComponent) {
+
+    }
+}
