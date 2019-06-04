2c2fac0f106ee9bce2da8f4e2306cb7e6003ec5b
noemicardillo <noemi.cardillo01@universitadipavia.it>
2019-06-04 14:44:01 +0200

    sistemata customizationMenuGUI

--- flappy/src/graphics/GUI/CustomizationMenuGUI.java
+++ flappy/src/graphics/GUI/CustomizationMenuGUI.java
@@ -25,20 +25,21 @@ public class CustomizationMenuGUI extends AbstractMenuGUI {
     public CustomizationMenuGUI(GameContainer container, Screen screen, CustomizationMenu state) throws SlickException {
         super(container, screen);
         this.state = state;
-        buttonHeight = container.getHeight()/7;
-        buttonWidth = container.getWidth()/7;
+        buttonHeight = container.getHeight()/9;
+        buttonWidth = container.getWidth()/9;
         Image batman = new Image("res/sprites/customTextures/batman/batman.png").getScaledCopy(buttonWidth,buttonHeight);
-        batmanButton = new MouseOverArea(container, batman, container.getWidth()/2 - 3*buttonWidth/2, container.getHeight()/2, buttonWidth, buttonHeight, this);
+        batmanButton = new MouseOverArea(container, batman, container.getWidth()/2 - 3*buttonWidth/2, 30*container.getHeight()/100, buttonWidth, buttonHeight, this);
         Image dogo = new Image("res/sprites/customTextures/dogo/dogo.png").getScaledCopy(buttonWidth,buttonHeight);
-        dogoButton = new MouseOverArea(container, dogo, container.getWidth()/2 - buttonWidth/2, container.getHeight()/2, buttonWidth, buttonHeight, this);
+        dogoButton = new MouseOverArea(container, dogo, container.getWidth()/2 - buttonWidth/2, 30*container.getHeight()/100, buttonWidth, buttonHeight, this);
         Image bird = new Image("res/sprites/player/bird.png").getScaledCopy(buttonWidth,buttonHeight);
-        classicButton = new MouseOverArea(container, bird, container.getWidth()/2 - 5*buttonWidth/2, container.getHeight()/2, buttonWidth, buttonHeight, this);
+        classicButton = new MouseOverArea(container, bird, container.getWidth()/2 - 5*buttonWidth/2, 30*container.getHeight()/100, buttonWidth, buttonHeight, this);
         Image fish = new Image("res/sprites/customTextures/sea/fish.png").getScaledCopy(buttonWidth,buttonHeight);
-        seaButton = new MouseOverArea(container, fish, container.getWidth()/2 + 3*buttonWidth/2, container.getHeight()/2, buttonWidth, buttonHeight, this);
+        seaButton = new MouseOverArea(container, fish, container.getWidth()/2 + 3*buttonWidth/2, 30*container.getHeight()/100, buttonWidth, buttonHeight, this);
         Image blueBird = new Image("res/sprites/customTextures/sky/bird.png").getScaledCopy(buttonWidth,buttonHeight);
-        skyButton = new MouseOverArea(container, blueBird, container.getWidth()/2 + 5*buttonWidth/2, container.getHeight()/2, buttonWidth, buttonHeight, this);
-        Image goBack = new Image("res/sprites/buttons/back.png").getScaledCopy(buttonWidth,buttonHeight);
-        returnButton = new MouseOverArea(container, goBack, container.getWidth()/2 - buttonWidth/2, container.getHeight()/4, buttonWidth, buttonHeight, this);
+        skyButton = new MouseOverArea(container, blueBird, container.getWidth()/2 + buttonWidth/2, 30*container.getHeight()/100, buttonWidth, buttonHeight, this);
+
+        Image goBack = new Image("res/sprites/buttons/back.png").getScaledCopy(buttonWidth*3,buttonHeight);
+        returnButton = new MouseOverArea(container, goBack, container.getWidth()/2 - buttonWidth/2, 75*container.getHeight()/100, buttonWidth, buttonHeight, this);
 
     }
 
