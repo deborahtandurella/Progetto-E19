7771db0b305bab9e100c3d06d5fc55d89c6cfa04
fGuareschi <federico.guareschi01@universitadipavia.it>
2019-07-07 22:42:01 +0200

    fix di ObstacleGenerator

--- flappy/src/game/itemGeneration/obstacle/NormalObstacleGenerator.java
+++ flappy/src/game/itemGeneration/obstacle/NormalObstacleGenerator.java
@@ -3,11 +3,18 @@ package game.itemGeneration.obstacle;
 import entityComponent.Entity;
 import entityComponent.EntityFactory;
 import graphics.Canvas;
+import logic.gameConstants.GameConstants;
 
 
+import java.util.ArrayList;
 import java.util.Random;
 
+import static logic.gameConstants.GameConstants.PIPE_SPEED;
+import static logic.gameConstants.GameConstants.PIPE_WIDTH;
+
 public class NormalObstacleGenerator extends ObstacleGenerator {
+
+
     protected NormalObstacleGenerator(Canvas canvas) {
         super(canvas);
     }
@@ -17,8 +24,13 @@ public class NormalObstacleGenerator extends ObstacleGenerator {
         Entity obstacle;
         if ( (new Random()).nextInt(5) > 2 )
             obstacle = EntityFactory.makeNormalPipe(1,0.25 + (new Random()).nextFloat() * 0.5, getCanvas());
-        else
-            obstacle = EntityFactory.makeMovingPipe(1,0.25 + (new Random()).nextFloat() * 0.5, getCanvas());
+        else {
+            double speedY = GameConstants.PIPE_VERTICAL_SPEED;
+            if(new Random().nextBoolean()){
+                speedY = -speedY;
+            }
+            obstacle = EntityFactory.makeMovingPipe(1,0.25 + (new Random()).nextFloat() * 0.5, speedY, getCanvas());
+        }
         notifyListeners(obstacle);
     }
 }
