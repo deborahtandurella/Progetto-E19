8ab052ae276428c5cf0c78ecd0298bbcb2587c9c
jacopoedoardodelcol <48446166+jacopoedoardodelcol@users.noreply.github.com>
2019-07-18 20:57:48 +0200

    eliminato classe DrawableElement inutilizzata

--- prototipoFlappy/src/elementi/DrawableElement.java
+++
@@ -1,9 +0,0 @@
-package elementi;
-
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-
-public interface DrawableElement {
-    public void update(GameContainer container, int delta);
-    public void render(GameContainer container, Graphics g);
-}
